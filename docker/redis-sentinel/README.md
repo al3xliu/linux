7c9778b44669ebcd136a4ef4ba11ec8aede180e3
alex <alexliusch@gmail.com>
2018-06-27 14:26:12 +0800

    add single&cluster doc

---
+++ docker/redis-sentinel/README.md
@@ -0,0 +1,13 @@
+# Redis HA in Docker
+
+## Introduction
+
+This document explains how to set up a high-available redis master-slave cluster base on `redis sentinel`, you can review sentinel official document https://redis.io/topics/sentinel. The following content will lead you to set up them both in test env(one node) and production environment(distributed).
+
+## Running on One Node
+
+
+
+## Running on Distributed Env(Docker Swarm)
+
+## How Does Client Connect to Them(Java)
--- docker/redis-sentinel/start.sh
+++ docker/redis-sentinel/start.sh
@@ -3,14 +3,27 @@
 set -e
 
 # Run docker master
-docker run --name="redis-master" -p 6379:6379 -d redis:3
+docker run \
+--name="redis-master" \
+--network=host \
+-d redis:3.2
 
-docker run --name="redis-slave1" -p 6380:6379 -d redis:3 redis-server --slaveof 192.168.1.5 6379
+docker run --name="redis-slave1" \
+--network=host \
+-v `pwd`/conf/slave1.conf:/usr/local/etc/redis/redis.conf \
+-d redis:3.2 \
+redis-server /usr/local/etc/redis/redis.conf --slaveof 127.0.0.1 6379
 
-docker run --name="redis-slave2" -p 6381:6379 -d redis:3 redis-server --slaveof 192.168.1.5 6379
+docker run --name="redis-slave2" \
+--network=host \
+-v ./conf/slave2.conf:/usr/local/etc/redis/redis.conf \
+-d redis:3.2 \
+redis-server /usr/local/etc/redis/redis.conf --slaveof 192.168.15.119 6379
 
 docker run -d --name='sentinel' \
--p 26379:26379 \
+--network=host \
 -e SENTINEL_DOWN_AFTER=5000 \
 -e SENTINEL_FAILOVER=5000 \
+-e SENTINEL_QUORUM=1 \
+-e MASTER_IP=127.0.0.1 \
 sentinel
